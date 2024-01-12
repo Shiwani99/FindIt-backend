95c52396711c126cc1c93d82a6e08fb8cf7ff48b
Shiwani Rajagopalan <rajagopalanshiwani@gmail.com>
2024-01-12 14:54:18 -0500

    initial commit

---
+++ src/test/java/com/findit/findit/FinditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findit.findit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
